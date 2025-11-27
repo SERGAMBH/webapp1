bf95e785c62b985e0ba377f9c088e36cf4ff5ce3
SERGAMBH <emkserg@gmail.com>
2025-11-27 12:06:43 +0300

    Create TodoContext.cs

---
+++ webapi/Models/TodoContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace webapi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+       : base(options)
+        {
+        }
+
+        //public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+        public DbSet<TodoItem> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoItem>(entity =>
+            {
+                entity.ToTable("items");
+                entity.HasKey(e => e.Name); // Указываем что Name - первичный ключ
+                entity.Property(e => e.Name)
+                      .HasMaxLength(100)
+                      .IsRequired(); // Обязательное поле
+            });
+        }
+    }
+}
